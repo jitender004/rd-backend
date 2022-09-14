337451ee030981f621e837bdca7ba9895cbef26f
jitender004 <jitenderjattu04@gmail.com>
2022-09-14 16:56:03 +0530

    rd-completed

--- src/model/products.model.js
+++ src/model/products.model.js
@@ -1,24 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ProductSchema =  new mongoose.Schema({
+const ProductSchema = new mongoose.Schema(
+  {
     // id:{type:Number,required:false},
-    title:{type:String, required:false} ,         //best seller
-    img:{type:String,required:true},           //image
-    name :{type:String,required:true},            //product name
-    description :{type:String,required:true} ,      //product description
-    price :{type:Number,required:true},             //product price
-    rating :{type:Number,required:true} ,
-
+    title: { type: String, required: false }, //best seller
+    img: { type: String, required: true }, //image
+    name: { type: String, required: true }, //product name
+    description: { type: String, required: true }, //product description
+    price: { type: Number, required: true }, //product price
+    rating: { type: Number, required: true },
+    catagory: {
+      type: String,
+      enum: [
+        "Regimen",
+        "faceScrum",
+        "action",
+        "eyes",
+        "casual",
+        
+      ],
+      default: "freeToPlay",
+    },
     //for single product page
-    color :{type:String},
-    details :{type:String} ,
-    size:{type:Number},
-    qty:{type:Number}
-},{
-    timestamps:true,
-    versionKey:false,
-})
+    color: { type: String },
+    details: { type: String },
+    size: { type: Number },
+    qty: { type: Number },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-const Product = mongoose.model('Product',ProductSchema);
+const Product = mongoose.model("Product", ProductSchema);
 
 module.exports = Product;
